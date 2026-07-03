1c4a8b0229d30fdf26f90d7aab355d6cf6f17d8b
zeynab narimani <lari.narimani.zeinab@gmail.com>
2026-07-03 16:59:06 +1000

    Fix do_twice decorator implementation

--- 8_decorators.py
+++ 8_decorators.py
@@ -1,9 +1,10 @@
 def do_twice(func):
-    func
-    func
-    return func
+    def wrapper():
+        func()
+        func()
+    return wrapper
 @do_twice
 def print_role():
     print("I am submissive to miss Leila and miss Sara")
 
-print(do_twice(print_role()))
+do_twice(print_role())
